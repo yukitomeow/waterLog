6f530e3c83ee48f4eb466af0bbc9d5150b08f05d
Yukimi Otagiri <YukimiOtagiri@Yukimis-MacBook-Pro.local>
2023-08-03 21:44:31 +0900

    added test and view

--- waterLog/water/tests.py
+++ waterLog/water/tests.py
@@ -3,4 +3,11 @@ from django.http import HttpRequest
 from water.views import top
 
 
+class TopPageViewTest(TestCase):
+    def test_top_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
--- waterLog/waterLog/urls.py
+++ waterLog/waterLog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from water.views import top
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",top,name="top"),
 ]
