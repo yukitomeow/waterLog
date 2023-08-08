292d3de26d422d80d88eab60b42b2784844f972d
Yukimi Otagiri <YukimiOtagiri@Yukimis-MacBook-Pro.local>
2023-08-08 15:33:03 +0900

    removed many files

--- waterLog/water/urls.py
+++ waterLog/water/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from water import views
 
 urlpatterns =[
-    path("new/", views.top_new, name="top_new"),
-    # path("edit/<int:water_id>/",views.edit_water_consumption, name="edit_water_consumption")
+   
 ]
