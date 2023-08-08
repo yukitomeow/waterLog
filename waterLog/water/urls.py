a2962bdc272c93e9ee45db4c7205e4f9510f612e
Yukimi Otagiri <YukimiOtagiri@Yukimis-MacBook-Pro.local>
2023-08-08 15:29:21 +0900

    modified a vies.py and top.htmls

--- waterLog/water/urls.py
+++ waterLog/water/urls.py
@@ -3,5 +3,5 @@ from water import views
 
 urlpatterns =[
     path("new/", views.top_new, name="top_new"),
-    path("edit/<int:water_id>/",views.edit_water_consumption, name="edit_water_consumption")
+    # path("edit/<int:water_id>/",views.edit_water_consumption, name="edit_water_consumption")
 ]
--- waterLog/water/views.py
+++ waterLog/water/views.py
@@ -13,19 +13,54 @@ def top(request, username):
         return HttpResponseForbidden("You don't have permission to view this.")
 
     today = date.today()
+    
     # Retrieve today's total water consumption for the user
     water_today = WaterConsumption.objects.filter(user=request.user, date=today).aggregate(total=Sum('amount_drank'))['total'] or 0
 
-    # If no water consumed yet, redirect to the input form
-    if not water_today:
-        return redirect('top_new')
+    if request.method == "POST":
+        form = WaterConsumptionForm(request.POST)
+        if form.is_valid():
+            water = form.save(commit=False)
+            water.user = request.user
+            water.date = today  # set today's date
+            water.save()
+            
+            # Recalculate the water consumed today after saving the new record
+            water_today = WaterConsumption.objects.filter(user=request.user, date=today).aggregate(total=Sum('amount_drank'))['total'] or 0
+
+    form = WaterConsumptionForm()  # Always provide an empty form for adding new water consumption
 
     context = {
         "total_water_today": water_today,
         "username": username,
         "date": today,
+        "form": form
     }
+
     return render(request, "water/top.html", context)
+
+
+
+# @login_required
+# def top(request, username):
+#     # Ensure the logged-in user can only see their records
+#     if request.user.username != username:
+#         return HttpResponseForbidden("You don't have permission to view this.")
+
+#     today = date.today()
+#     # Retrieve today's total water consumption for the user
+#     water_today = WaterConsumption.objects.filter(user=request.user, date=today).aggregate(total=Sum('amount_drank'))['total'] or 0
+
+#     # If no water consumed yet, redirect to the input form
+#     if not water_today:
+#         return redirect('top_new')
+
+#     context = {
+#         "total_water_today": water_today,
+#         "username": username,
+#         "date": today,
+#     }
+#     return render(request, "water/top.html", context)
 
 @login_required
 def top_new(request):
