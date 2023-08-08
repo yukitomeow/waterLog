1807ccee7125a00200f7ee8493244dbbd759320a
Yukimi Otagiri <YukimiOtagiri@Yukimis-MacBook-Pro.local>
2023-08-08 16:00:04 +0900

    Revert "removed many files"
    This reverts commit 292d3de26d422d80d88eab60b42b2784844f972d.

--- waterLog/water/urls.py
+++ waterLog/water/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from water import views
 
 urlpatterns =[
-   
+    path("new/", views.top_new, name="top_new"),
+    # path("edit/<int:water_id>/",views.edit_water_consumption, name="edit_water_consumption")
 ]
--- waterLog/water/views.py
+++ waterLog/water/views.py
@@ -41,4 +41,45 @@ def top(request, username):
 
 
 
+# @login_required
+# def top(request, username):
+#     # Ensure the logged-in user can only see their records
+#     if request.user.username != username:
+#         return HttpResponseForbidden("You don't have permission to view this.")
+
+#     today = date.today()
+#     # Retrieve today's total water consumption for the user
+#     water_today = WaterConsumption.objects.filter(user=request.user, date=today).aggregate(total=Sum('amount_drank'))['total'] or 0
+
+#     # If no water consumed yet, redirect to the input form
+#     if not water_today:
+#         return redirect('top_new')
+
+#     context = {
+#         "total_water_today": water_today,
+#         "username": username,
+#         "date": today,
+#     }
+#     return render(request, "water/top.html", context)
+
+@login_required
+def top_new(request):
+    today = date.today()
+    
+    # If there's a POST request, try to save the new water consumption
+    if request.method == "POST":
+        form = WaterConsumptionForm(request.POST)
+        if form.is_valid():
+            water = form.save(commit=False)
+            water.user = request.user
+            water.date = today  # set today's date
+            water.save()
+            return redirect("top", username=request.user.username)
+
+    else:
+        form = WaterConsumptionForm()
+
+    return render(request, "water/top_new.html", {"form": form})
+
+
 
