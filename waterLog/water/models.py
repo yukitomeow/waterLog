89af58d61ce061e6733e23d3d14b1737d2de407d
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2023-08-21 17:49:41 +0900

    Deleted unusde file

--- waterLog/water/models.py
+++ waterLog/water/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.utils.translation import gettext as _
+
 
 class WaterConsumption(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount_drank = models.FloatField(default=0)
     date = models.DateField(default=timezone.now)
 
-    # @staticmethod
-    # def find_by_user_username(username):
-    #     return WaterConsumption.objects.get(user__username=username)
-        
     def __str__(self):
-        return f"User: {self.user}, Amount Drank: {self.amount_drank}, Date: {self.date}"
-
+        return f"User: {self.user}, {_('Amount Drank')}: {self.amount_drank}, Date: {self.date}"
+        
+   
