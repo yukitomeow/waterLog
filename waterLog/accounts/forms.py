96ea54f43cebd5b7253305c00ce73427d7c42035
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2023-08-31 16:42:40 +0900

    registration form

--- waterLog/accounts/forms.py
+++ waterLog/accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.utils.translation import gettext_lazy as _
+import re
 
 
 
@@ -19,15 +20,16 @@ class RegistrationForm(UserCreationForm):
         label=_("Unit"),
         help_text=_("Please select your preferred unit.")
     )
-
-
-
-
-# class RegistrationForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-#     unit = forms.ChoiceField(choices=UserProfile.UNIT_CHOICES, widget=forms.RadioSelect, initial='ml')
-
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2", "unit"]
 
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            clean_help_text = field.help_text
+            if isinstance(clean_help_text, str):  # Check if it's a string
+                clean_help_text = re.sub(r'<.*?>', '', clean_help_text)
+            field.widget.attrs['title'] = clean_help_text
+            field.widget.attrs['data-bs-toggle'] = 'tooltip'
+            field.help_text = ''
