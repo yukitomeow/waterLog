655af185e52df13f3fdfc641962889d7028e74fc
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2023-09-02 18:15:39 +0900

    deleted label from the form, and make it as a place holder

--- waterLog/water/forms.py
+++ waterLog/water/forms.py
@@ -3,7 +3,9 @@ from water.models import WaterConsumption
 from django.forms import ModelForm
 
 class WaterConsumptionForm(ModelForm):
+    amount_drank = forms.FloatField(label='', widget=forms.TextInput(attrs={'placeholder': 'Amount Drank'}))
+
     class Meta:
-        model= WaterConsumption
-        fields=('amount_drank',)
+        model = WaterConsumption
+        fields = ('amount_drank',)
 
