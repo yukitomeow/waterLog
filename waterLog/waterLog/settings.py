7ddaa5f64dbca2742f1f6558bdef1bcb8550c14a
Yukimi Otagiri <Yukimi.otagiri@gmail.com>
2023-09-09 14:50:59 +0900

    deploy to fly.io

--- waterLog/waterLog/settings.py
+++ waterLog/waterLog/settings.py
@@ -15,8 +15,6 @@ import os
 from django.utils.translation import gettext_lazy as _
 import dj_database_url
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%wd3yft6e-ca3sh!q)z(w$$n0f_5=@da0efat-=rgueld_htw7"
+# SECRET_KEY = "django-insecure-%wd3yft6e-ca3sh!q)z(w$$n0f_5=@da0efat-=rgueld_htw7"
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-%wd3yft6e-ca3sh!q)z(w$$n0f_5=@da0efat-=rgueld_htw7")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
